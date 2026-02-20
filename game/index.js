c0e00c36288bdb5ba86edf8c11ff868722cd5378
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2026-02-20 09:27:26 -0300

    assests for enemies and effects added and implemented

--- game/index.js
+++ game/index.js
@@ -5,8 +5,8 @@ import { Background } from "./Background.js";
 import { UI } from "./UI.js";
 import { Layer } from "./Layer.js";
 import { InputHandler } from "./InputHandler.js";
-import { Enemy } from "./Enemy.js";
-import Anglers from "./Anglers.js";
+import Enemy from "./Enemy.js";
 import { Game } from "./Game.js";
+import Effects from "./Effects.js";
 
-export {Player, Particle, Projectile, Background, UI, Layer, InputHandler, Enemy, Game, Anglers}
+export {Player, Particle, Projectile, Background, UI, Layer, InputHandler, Enemy, Game, Effects}
