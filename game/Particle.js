7bd6c3481b2fd5146be855c512e6e17fe247e168
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2026-02-20 09:25:17 -0300

    refine particles to have a rotation

--- game/Particle.js
+++ game/Particle.js
@@ -23,7 +23,7 @@ export class Particle {
   update() {
     this.angle += this.va;
     this.speedY += this.gravity;
-    this.x -= this.speedX;
+    this.x -= this.speedX + this.game.speed;
     this.y += this.speedY;
 
     if (this.y > this.game.height + this.size || this.x < 0 - this.size)
@@ -37,16 +37,20 @@ export class Particle {
   }
 
   draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
     context.drawImage(
       this.image,
       this.frameX * this.spriteSize,
       this.frameY * this.spriteSize,
       this.spriteSize,
       this.spriteSize,
-      this.x,
-      this.y,
+      this.size * -0.5,
+      this.size * -0.5,
       this.size,
       this.size
-    )
+    );
+    context.restore();
   }
 }
